1f1eb9b18a7153988468f0e91de0d3bb0b7d7031
Andrew Gadion <andrew.gadion@gmail.com>
2021-12-01 23:47:43 +0200

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
+    fun countIncreasedNumbers(input: Sequence<Int>) = input.zip(input.drop(1)).count { it.second > it.first }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputNumbers = input
+            .asSequence()
+            .map { it.toInt() }
+        return countIncreasedNumbers(inputNumbers)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputNumbers = input.map { it.toInt() }
+        val sums = (0 until input.size - 2).asSequence().map {
+            inputNumbers[it] + inputNumbers[it + 1] + inputNumbers[it + 2]
+        }
+        return countIncreasedNumbers(sums)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day1")
     println(part1(input))
     println(part2(input))
 }
